e6dbb6df7f6315b661db61aca46d4cd001a0f34a
ithalloelzo <ithalloelzo@gmail.com>
2024-03-11 12:27:32 -0300

    quartocommit

--- index.js
+++ index.js
@@ -1,25 +1,97 @@
-const nametag = document.getElementById("name").value
-const adress = document.getElementById("adress").value
-const gender = document.getElementById("gender").value
-const contact = document.getElementById("contact").value
-const age = document.getElementById("age").value
-const classtag = document.getElementById("class").value
+class CreateUser {
+    constructor(id, nametag, age, gender, classtag, contact, adress) {
+        this.id = id
+        this.name = nametag
+        this.age = age
+        this.gender = gender
+        this.class = classtag
+        this.contact = contact
+        this.adress = adress
+    }
+}
+let arrayUser = []
+
 const register = document.getElementById("register")
 const tab = document.getElementById("tab")
 
-
-function addUser(){
-
-    tab.innerHTML = `
-    <tr>  
-    <th>${nametag}</th>
-    <th>${age}</th>
-    <th>${gender}</th>
-    <th>${classtag}</th>
-    <th>${contact}</th>
-    <th>${adress}</th>
-    </tr> `
-    
+function generateID() {
+    return Math.random().toString(36).substr(2, 5);
 }
+
+function addUser() {
+    const nametag = document.getElementById("name").value
+    const adress = document.getElementById("adress").value
+    const gender = document.getElementById("gender").value
+    const contact = document.getElementById("contact").value
+    const age = document.getElementById("age").value
+    const classtag = document.getElementById("class").value
+
+    const id = generateID()
+
+    const newUser = new CreateUser(id, nametag, age, gender, classtag, contact, adress)
+
+    arrayUser.push(newUser)
+
+    tab.innerHTML += `
+    <tr id="${newUser.id}">  
+    <th>${newUser.name}</th>
+    <th>${newUser.age}</th>
+    <th>${newUser.gender}</th>
+    <th>${newUser.class}</th>
+    <th>${newUser.contact}</th>
+    <th>${newUser.adress}</th>
+    <th>
+     <button onclick="deleteUser('${newUser.id}')">Excluir</button>
+     <button onclick="editUser('${newUser.id}')">Editar</button>
+    </th>
+    </tr> `
+}
+
+function deleteUser(id) {
+    const usertag = document.getElementById(id)
+    usertag.remove()
+    let indexId = arrayUser.findIndex(user => user.id === id)
+    arrayUser.splice(indexId, 1)
+}
+
+function closeShow(){
+    const editDisplay = document.getElementById('editDisplay')
+    let setDisplay = editDisplay.style.display == 'none' ? 'flex' : 'none'
+    editDisplay.style.display = setDisplay
+}
+
+
+function editUser(id) {
+    let buttonEdit = document.getElementsByClassName("buttonEdit")[0]
+    buttonEdit.setAttribute("onclick", `updateUser(${id})`)
+    closeShow()
+    let indexId = arrayUser.findIndex(user => user.id === id);
+
+
+    document.getElementById("nameEdit").value = arrayUser[indexId].name
+    document.getElementById("ageEdit").value = arrayUser[indexId].age
+    document.getElementById("genderEdit").value = arrayUser[indexId].gender
+    document.getElementById("classEdit").value = arrayUser[indexId].class
+    document.getElementById("contactEdit").value = arrayUser[indexId].contact
+    document.getElementById("adressEdit").value = arrayUser[indexId].adress
+}
+
+function updadeUser(id){
+    const nametag = document.getElementById("nameEdit").value
+    const age = document.getElementById("ageEdit").value
+    const gender = document.getElementById("genderEdit").value
+    const classtag = document.getElementById("classEdit").value
+    const contact = document.getElementById("contactEdit").value
+    const adress = document.getElementById("adressEdit").value
+    
+    const editUser = new CreateUser(id, nametag, age, gender, classtag, contact, adress)
+    
+    let indexId = arrayUser.findIndex(user => user.id === id);
+
+    arrayUser[indexId] = editUser
+
+}
+
+
 
 
