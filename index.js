e09ce74816563e6cc08edc16587b707ac2ff68f9
ithalloelzo <ithalloelzo@gmail.com>
2024-03-11 22:20:35 -0300

    Registro de Dados

--- index.js
+++ index.js
@@ -9,10 +9,31 @@ class CreateUser {
         this.adress = adress
     }
 }
-let arrayUser = []
+var userSave = JSON.parse(localStorage.getItem("userSave"))
+var arrayUser = userSave == null ? [] : userSave
+
+localStorage.setItem("userSave", JSON.stringify(arrayUser))
 
 const register = document.getElementById("register")
 const tab = document.getElementById("tab")
+
+function restUser() {
+    for (let user of arrayUser) {
+        tab.innerHTML += `
+        <tr id="${user.id}">  
+        <th>${user.name}</th>
+        <th>${user.age}</th>
+        <th>${user.gender}</th>
+        <th>${user.class}</th>
+        <th>${user.contact}</th>
+        <th>${user.adress}</th>
+        <th>
+         <button onclick="deleteUser('${user.id}')">Excluir</button>
+         <button onclick="editUser('${user.id}')">Editar</button>
+        </th>
+        </tr> `
+    }
+}
 
 function generateID() {
     return Math.random().toString(36).substr(2, 5);
@@ -25,11 +46,11 @@ function addUser() {
     const contact = document.getElementById("contact").value
     const age = document.getElementById("age").value
     const classtag = document.getElementById("class").value
-    if ((nametag.length || adress.length || gender.length || contact.length || age.length || classtag.length) < 3) {
+    if (nametag.length < 3 || adress.length < 3 || contact.length < 3 || age.length < 2 || classtag.length < 3) {
         alert("Preencha com no mínimo 3 caracteres")
 
     }
-    else if (contact) {
+    else if (contact.includes("@") != true) {
         alert("Preencha o email corretamente")
 
     }
@@ -53,6 +74,7 @@ function addUser() {
      <button onclick="editUser('${newUser.id}')">Editar</button>
     </th>
     </tr> `
+        localStorage.setItem("userSave", JSON.stringify(arrayUser))
     }
 
 }
@@ -62,6 +84,8 @@ function deleteUser(id) {
     usertag.remove()
     let indexId = arrayUser.findIndex(user => user.id === id)
     arrayUser.splice(indexId, 1)
+
+    localStorage.setItem("userSave", JSON.stringify(arrayUser))
 }
 
 function closeShow() {
@@ -94,22 +118,36 @@ function updateUser(id) {
     const contact = document.getElementById("contactEdit").value
     const adress = document.getElementById("adressEdit").value
 
-    const editUser = new CreateUser(id, nametag, age, gender, classtag, contact, adress)
+    if (nametag.length < 3 || adress.length < 3 || contact.length < 3 || age.length < 2 || classtag.length < 3) {
+        alert("Preencha com no mínimo 3 caracteres")
 
-    let indexId = arrayUser.findIndex(user => user.id === id);
+    }
+    else if (contact.includes("@") != true) {
+        alert("Preencha o email corretamente")
 
-    arrayUser[indexId] = editUser
-    let userTab = document.getElementById(id)
-    let userTabTh = userTab.getElementsByTagName("th")
-    userTabTh[0].innerText = editUser.name
-    userTabTh[1].innerText = editUser.age
-    userTabTh[2].innerText = editUser.gender
-    userTabTh[3].innerText = editUser.class
-    userTabTh[4].innerText = editUser.contact
-    userTabTh[5].innerText = editUser.adress
-    closeShow()
+    }
+    else {
+
+        const editUser = new CreateUser(id, nametag, age, gender, classtag, contact, adress)
+
+        let indexId = arrayUser.findIndex(user => user.id === id);
+
+        arrayUser[indexId] = editUser
+        let userTab = document.getElementById(id)
+        let userTabTh = userTab.getElementsByTagName("th")
+        userTabTh[0].innerText = editUser.name
+        userTabTh[1].innerText = editUser.age
+        userTabTh[2].innerText = editUser.gender
+        userTabTh[3].innerText = editUser.class
+        userTabTh[4].innerText = editUser.contact
+        userTabTh[5].innerText = editUser.adress
+        closeShow()
+
+        localStorage.setItem("userSave", JSON.stringify(arrayUser))
+    }
+
 }
-
+restUser() 
 
 
 
